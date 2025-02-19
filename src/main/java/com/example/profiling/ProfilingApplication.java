310abf87209dab621d0a47f224e588e3463919e0
HotelApp <expatriateadodo@gmail.com>
2025-02-20 00:52:01 +0200

    Working

--- src/main/java/com/example/profiling/ProfilingApplication.java
+++ src/main/java/com/example/profiling/ProfilingApplication.java
@@ -3,7 +3,9 @@ package com.example.profiling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 
@@ -18,4 +20,15 @@ public class ProfilingApplication {
 public void welcomePage() {
         System.out.println("Welcome to Profiling");
 }
+// internationalization it has 20 characters = i18n
+    @Bean
+    public ReloadableResourceBundleMessageSource getMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    };
+
+
+
 }
