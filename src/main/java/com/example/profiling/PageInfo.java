2bec1c22330d23b7f33aadebca7b4e460c215aa8
HotelApp <expatriateadodo@gmail.com>
2025-02-19 22:23:43 +0200

    Using a @Value and, @ConfigurationProperties

---
+++ src/main/java/com/example/profiling/PageInfo.java
@@ -0,0 +1,24 @@
+package com.example.profiling;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+@Setter
+@Component
+@ConfigurationProperties(prefix = "page-info")
+public class PageInfo {
+  //  @Value("${page_info.author}")
+    private String author;//null
+   // @Value("${page_info.date}")
+    private int creationDate ; //0
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void print() {
+        System.out.println("Author:" + author);
+        System.out.println("Creation date: " + creationDate);
+    }
+}
